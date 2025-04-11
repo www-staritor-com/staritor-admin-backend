894185e039562202ec804301367bbd7d36199ab6
yui <yui@apache.org>
2025-04-12 00:13:02 +0800

    add: 增加全局异常捕获

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,9 +1,22 @@
+use crate::entity::base::response::Response;
 use rocket::fairing::AdHoc;
+use rocket::http::Status;
+use rocket::Request;
 
 mod admin;
 
+#[catch(default)]
+fn default_catch_all(status: Status, req: &Request<'_>) -> Response<()> {
+    Response::error(status.code, req.uri().to_string())
+}
+
 pub fn stage(prefix: String) -> AdHoc {
     AdHoc::on_ignite("Controller Init", move |rocket| async move {
-        rocket.attach(admin::stage(format!("{}/{}", prefix.clone(), "api")))
+        rocket
+            .register(
+                format!("{}/{}", prefix.clone(), "api"),
+                catchers![default_catch_all],
+            )
+            .attach(admin::stage(format!("{}/{}", prefix.clone(), "api")))
     })
 }
